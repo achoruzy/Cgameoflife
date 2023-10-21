785678c295a60e54fa5ad76516938ace303f0db7
industArk <achoruzy@gmail.com>
2023-10-21 16:52:18 +0200

    implemented custom grid builder

--- main.c
+++ main.c
@@ -1,5 +1,8 @@
 #include <stdio.h>
 #include <raylib.h>
+#include <raymath.h>
+#include <rlgl.h>
+#include "./src/utils/drawUtils.c"
 
 int main()
 {
@@ -8,24 +11,35 @@ int main()
 
 	Color bgColor = { 21, 30, 39, 255 };
 
-
+	SetConfigFlags(FLAG_WINDOW_RESIZABLE | FLAG_VSYNC_HINT);
 	InitWindow(windowWidth, windowHeight, "C Game of Life");
+	SetWindowMinSize(windowWidth, windowHeight);
 	SetTargetFPS(60);
 
 	Camera2D mainCamera = {0};
+	mainCamera.target =  (Vector2){ 0.0f, 0.0f };
 	mainCamera.zoom = 1.f;
 
 	while (!WindowShouldClose())
     {
+		if (IsKeyPressed(299)) ToggleFullscreen(); // TODO: resize window with fullscreen and get back
 		// get input
 		// use input (UI logic and gameplay)
 
 		BeginDrawing();
+		BeginMode2D(mainCamera);
 		ClearBackground(bgColor);
 		// draw game
 			// grid matrix
+		rlPushMatrix();
+			rlTranslatef(0, 25*50, 0);
+			rlRotatef(90, 1, 0, 0);
+			DrawCustomGrid(100, 50, (Vector3){0.2, 0.2, 0.7}, 2.f);
+		rlPopMatrix();
 			// grid lines (with turn off)
 			// spawning cells
+		DrawCircle(100, 100, 50, YELLOW);
+		EndMode2D();
 		// postprocess
 		// draw UI
 		EndDrawing();
---
+++ src/utils/drawUtils.c
@@ -0,0 +1,35 @@
+#include <raylib.h>
+#include <rlgl.h>
+
+// Draw a grid
+static void DrawCustomGrid(int slices, float spacing, Vector3 color, float th)
+{
+    int halfSlices = slices / 2;
+
+    rlBegin(RL_LINES);
+    rlSetLineWidth(th);
+        for (int i = -halfSlices; i <= halfSlices; i++)
+        {
+            if (i == 0)
+            {
+                rlColor3f(0.5f, 0.5f, 0.5f);
+                rlColor3f(0.5f, 0.5f, 0.5f);
+                rlColor3f(0.5f, 0.5f, 0.5f);
+                rlColor3f(0.5f, 0.5f, 0.5f);
+            }
+            else
+            {
+                rlColor3f(0.75f, 0.75f, 0.75f);
+                rlColor3f(0.75f, 0.75f, 0.75f);
+                rlColor3f(0.75f, 0.75f, 0.75f);
+                rlColor3f(0.75f, 0.75f, 0.75f);
+            }
+
+            rlVertex3f((float)i*spacing, 0.0f, (float)-halfSlices*spacing);
+            rlVertex3f((float)i*spacing, 0.0f, (float)halfSlices*spacing);
+
+            rlVertex3f((float)-halfSlices*spacing, 0.0f, (float)i*spacing);
+            rlVertex3f((float)halfSlices*spacing, 0.0f, (float)i*spacing);
+        }
+    rlEnd();
+}
