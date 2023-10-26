cb17f347c85a611dc80a9ddb6f8b51da0e237f9a
industArk <achoruzy@gmail.com>
2023-10-26 20:39:14 +0200

    implemented cell neighbor counter

--- src/logic/cell.c
+++ src/logic/cell.c
@@ -11,4 +11,29 @@ Cell* CellArray(int size)
     return cellPtr;
 }
 
-// check neighbourhood -> calculate num of neighbours -> decide on rules
+int CellNeighborsQty(int cell_x, int cell_y, Cell* cellArray, int* cellArrayLengthPtr)
+{
+    /* cases
+    N -> x, y-1
+    S -> x, y+1
+    W -> x-1, y
+    E -> x+1, y
+    NW -> x-1, y-1
+    NE -> x+1, y-1
+    SW -> x-1, y+1
+    SE -> x+1, y+1
+    */
+    int count = 0;
+    for (int i = 0; i < *cellArrayLengthPtr - 1; i++)
+    {
+        int x = cellArray[i].x;
+        int y = cellArray[i].y;
+
+        if (x == cell_x && y == cell_y) continue;
+
+        if (x == cell_x || x == cell_x-1 || x == cell_x +1)
+        {
+            if (y == cell_y || y == cell_y-1 || y == cell_y +1) count++;
+        }
+    }
+}
