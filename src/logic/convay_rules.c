70591efd2f5037a8cbb29a2b90e0a1b66b5b32cd
industArk <achoruzy@gmail.com>
2023-10-29 15:45:40 +0100

    updated and simplified convay rules

--- src/logic/convay_rules.c
+++ src/logic/convay_rules.c
@@ -1,29 +1,32 @@
 // Copyright (C) Arkadiusz Choruży
 
-
 #include "convay_rules.h"
 
-bool TryObeyRules(Cell* cell, int num_neighbors)
+bool TryObeyRules(Cell *cell, int num_neighbors)
 {
-    // renew
-    if (cell->isDead == true && num_neighbors == 3) 
-    {
-        cell->isDead = false;
-        return true;
-    }
+    // revive
+    // if (cell->isDead == true && num_neighbors == 3)
+    // {
+    //     cell->isDead = false;
+    //     return true;
+    // }
 
     // kill definitely
-    if (cell->isDead == true) return false;
+    if (cell->isDead == true)
+        return false;
 
-    switch (num_neighbors)
-    {
-    case 2:
-    case 3:
-    cell->isDead = false;
-        break;
-    default: // kill temporary
-        cell->isDead = true;
-        break;
-    }
+    bool isCellToLive = isToLive(num_neighbors);
+    cell->isDead = isCellToLive;
     return true;
+}
+
+// When cell is empty but has 3 living cells around, then is to revive
+static bool isToRevive(int num_neighbors) { return num_neighbors == 3; }
+
+// When cell has 2 or 3 neighbors only it may live
+static bool isToLive(int num_neighbors)
+{
+    if (num_neighbors == 2 || num_neighbors == 3)
+        return true;
+    return false;
 }
