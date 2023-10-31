783c316c5769aa5812da1bc64ed8fe017afa11fe
industArk <achoruzy@gmail.com>
2023-10-31 19:10:07 +0100

    updated test suite

--- test/test_main.c
+++ test/test_main.c
@@ -3,7 +3,20 @@
 #include <stdio.h>
 #include "test_suite.h"
 
+#include "../src/logic/convay_rules.h"
+
 void main()
 {
-    TEST(ASSERT_EQ(2, 2), "Equals");
+    TEST_START();
+
+    TEST(ASSERT_TRUE(isToRevive(3)),
+         "convay_rules.c: isToRevive returns true if param ==3");
+    TEST(ASSERT_FALSE(isToRevive(2)),
+         "convay_rules.c: isToRevive returns false if param <3");
+    TEST(ASSERT_FALSE(isToRevive(4)),
+         "convay_rules.c: isToRevive returns false if param >3");
+    TEST(ASSERT_FALSE(isToRevive(3)),
+         "convay_rules.c: isToRevive returns false if param >3");
+
+    TEST_END();
 }
--- test/test_suite.h
+++ test/test_suite.h
@@ -5,19 +5,36 @@
 
 #define ANSI_COLOR_RED "\x1b[31m"
 #define ANSI_COLOR_GREEN "\x1b[32m"
+#define ANSI_COLOR_CYAN "\x1b[36m"
+#define ANSI_COLOR_RESET "\x1b[0m"
 
 // TESTING ASSERTIONS
 // true - false macros
 #define ASSERT_EQ(a, b) (a == b)
 #define ASSERT_NOT_EQ(a, b) (a != b)
+#define ASSERT_TRUE(a) (a)
+#define ASSERT_FALSE(a) (!a)
+
+void TEST_START()
+{
+    printf("\n");
+    printf(ANSI_COLOR_CYAN "TESTING STARTED" ANSI_COLOR_RESET "\n");
+    printf("---------------\n");
+}
+
+void TEST_END()
+{
+    printf("---------------\n");
+    printf("\n");
+}
 
 bool TEST(bool ASSERTION, char *title)
 {
     if (ASSERTION)
     {
-        printf(ANSI_COLOR_GREEN "Test passed: %s\n", title);
+        printf(ANSI_COLOR_GREEN "Test passed:" ANSI_COLOR_RESET " %s\n", title);
         return true;
     }
-    printf(ANSI_COLOR_RED "Test failed: %s\n", title);
+    printf(ANSI_COLOR_RED "Test failed: " ANSI_COLOR_RESET " %s\n", title);
     return false;
 }
